19ae1c120ed7036cfe522082ca056261b21f6b27
bigredpanda <me@jan-biederbeck.de>
2021-11-02 21:51:32 +0100

    Updated frontend

--- frontend.py
+++ frontend.py
@@ -35,13 +35,14 @@ def imports():
     vehicle_cost = pd.read_excel("TCO-trucking.xlsx", sheet_name="Vehicle cost")
     energy_cons = pd.read_excel("TCO-trucking.xlsx", sheet_name="Energy consumption")
     energy_cost = pd.read_excel("TCO-trucking.xlsx", sheet_name="Energy cost")
+    eu_country_cost = pd.read_excel("TCO-trucking.xlsx", sheet_name="EU electricity cost", header=11)
     toll_cost = pd.read_excel("TCO-trucking.xlsx", sheet_name="Toll")
     d = {'Year': [2020, 2025, 2030, 2040, 2050], 'Cost [€/t]': [0, 55, 100, 150, 200]}
     co2_price = pd.DataFrame(data=d)
-    return vehicle_cost, energy_cons, energy_cost, toll_cost, co2_price
+    return vehicle_cost, energy_cons, energy_cost, toll_cost, co2_price, eu_country_cost
 
 
-vehicle_cost, energy_cons, energy_cost, toll_cost, co2_price_l = imports()
+vehicle_cost, energy_cons, energy_cost, toll_cost, co2_price_l, eu_country_cost = imports()
 
 cons = energy_cons.copy()
 cost = vehicle_cost.copy()
@@ -80,6 +81,7 @@ st.sidebar.write("Models and assumptions for long-haul trucks (40 t) are based o
 
 container = st.container()
 container.title('TCO-Calculator for different Green Trucking Technologies')
+eu_country_cost = eu_country_cost.replace(":", 0)
 
 
 with st.expander("Policy framework", expanded=True):
@@ -91,6 +93,8 @@ with st.expander("Policy framework", expanded=True):
         with cols[i]:
             st.subheader(selected_options[i])
             toll.loc[ (toll["Vehicle"] == selected_options[i]) & (toll["Year"] == selected_year), "Cost [€/km]"] = st.number_input("Toll for infrastructure, noise and air pollution [€/km]", float(0), 0.5, toll_cost.loc[ (toll_cost["Vehicle"] == selected_options[i]) & (toll_cost["Year"] == selected_year), "Cost [€/km]"].item(), 0.01, key="fsdp"+str(i))
+
+container2 = st.container()
 
 with st.expander("Vehicle Cost"):
     num_years = st.slider("Period under observation [years]", 1, 5, 5, 1)
@@ -105,11 +109,13 @@ with st.expander("Vehicle Cost"):
             cost.loc[ (cost["Vehicle"] == selected_options[i]) & (cost["Cost type"] == "Maintenance & repair") & (cost["Year"] == selected_year), "Cost [€]"] = st.number_input("Annual maintenance & repair [t€]", float(2), float(1000), vehicle_cost.loc[ (vehicle_cost["Vehicle"] == selected_options[i]) & (vehicle_cost["Cost type"] == "Maintenance & repair")& (vehicle_cost["Year"] == selected_year), "Cost [€]"].item()/1000, float(1), key="vm"+str(i)) * 1000
 
 
-with st.expander("Vehicle Stats & Energy Consumption"):
+with st.expander("Vehicle Energy Consumption"):
     yearly_mileage = st.slider("Average annual mileage [km]", 10000, 300000,  136750, 5000)
     cols = st.columns(num_options)
+    st.markdown("All values tank/plug to wheel")
     for i in range(num_options):
         with cols[i]:
+
             st.subheader(selected_options[i])
             cons.loc[(cons["Vehicle"] == selected_options[i]) & (cons["Year"] == selected_year), "Consumption"] = st.number_input("Energy consumption [" + energy_cons.loc[(energy_cons["Vehicle"] == selected_options[i]) & (energy_cons["Year"] == selected_year), "Unit"].item() + "]", float(0), float(50), energy_cons.loc[(energy_cons["Vehicle"] == selected_options[i]) & (energy_cons["Year"] == selected_year), "Consumption"].item(), float(0.025))
 
@@ -121,7 +127,8 @@ with st.expander("Vehicle Stats & Energy Consumption"):
                 cons.loc[(cons["Vehicle"] == selected_options[i]) & (cons["Year"] == selected_year), "Unit"] = "kWh/km"
 
 
-with st.expander("Energy Cost"):
+
+with container2.expander("Energy Cost"):
     cols = st.columns(num_options)
     for i in range(num_options):
         with cols[i]:
@@ -187,11 +194,15 @@ cost_e.reset_index(inplace=True)
 
 cost = cost_e
 cost = cost.loc[(cost["Year"] == selected_year) & (cost["Vehicle"].isin(selected_options))]
-fig = px.bar(cost, x="Vehicle", y="Cost [€]",  color="Cost type",  hover_name="Vehicle", hover_data= ["Share of TCO"],color_discrete_sequence=px.colors.qualitative.T10, title="Total cost of ownership in first "+str(num_years)+" year use period")
+cost["Cost type"] = cost["Cost type"].replace({'Purchase cost':'Vehicle'})
+fig = px.bar(cost, x="Vehicle", y="Cost [€]",  color="Cost type",  hover_name="Vehicle", hover_data= ["Share of TCO"],color_discrete_sequence=px.colors.qualitative.T10, title="Total cost of ownership in first "+str(num_years)+" year use period<br><sup>Base year "+str(selected_year) +", excluding driver costs</sup>")
 
 fig.update_xaxes(categoryorder='array', categoryarray= selected_options)
 fig.update_layout(font_size = 15, title_x=0.5)
 
 # out of order displaying with containers
 container.write(fig)
+
+st.subheader("Sources")
+st.markdown("Eurostat (2021). [Electricity prices for non-household consumers - bi-annual data (from 2007 onwards)](https://ec.europa.eu/eurostat/databrowser/view/NRG_PC_205__custom_1376243/default/table?lang=en)\n\nTransport & Environment (2021). [How to decarbonise long-haul trucking in Germany. An analysis of available vehicle technologies and their associsated costs.](https://www.transportenvironment.org/wp-content/uploads/2021/07/2021_04_TE_how_to_decarbonise_long_haul_trucking_in_Germany_final.pdf)")
 
